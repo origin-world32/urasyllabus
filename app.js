cd278215240282a32b7c6e7e88e560fbd2b1139d
origin-world32 <onoshu1213@icloud.com>
2025-11-21 18:56:56 +0900

    Update app.js

--- app.js
+++ app.js
@@ -150,3 +150,10 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+})
